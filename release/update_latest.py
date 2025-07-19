3e88bd26b10a46c09ee89c08e958e290efb6d988
ALXS-GitHub <alxs.mnch@gmail.com>
2025-07-19 20:17:53 +0200

    latest_template.json

---
+++ release/update_latest.py
@@ -0,0 +1,2 @@
+# TODO make a script to fetch from the tauri target release folder for latest version
+# and create a new version of the latest.json file so we can place it on the latest release linked files.
